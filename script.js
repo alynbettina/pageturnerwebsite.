4b6748153864a20f5343385abb9cae21a25b880a
alynbettina <133723496+alynbettina@users.noreply.github.com>
2023-05-16 10:48:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+<!-- ---------Javascript for toggle menu------------- -->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.maxHeight = "0px";
+      function menutoggle() {
+        if (MenuItems.style.maxHeight == "0px") {
+          MenuItems.style.maxHeight = "200px";
+        } else {
+          MenuItems.style.maxHeight = "0px";
+        }
+      }
+    </script>
+    <!-- -----------js for toggle form------------------ -->
+    <script>
+      var signInForm = document.getElementById("signInForm");
+      var signUpForm = document.getElementById("signUpForm");
+      var indicator = document.getElementById("indicator");
+
+      function signIn() {
+        signUpForm.style.transform = "translateX(300px)";
+        signInForm.style.transform = "translateX(300px)";
+        indicator.style.transform = "translateX(0px)";
+      }
+      function signUp() {
+        signUpForm.style.transform = "translateX(0px)";
+        signInForm.style.transform = "translateX(0px)";
+        indicator.style.transform = "translateX(100px)";
+      }
+    </script>
+    <!-- -----------------js for form validation ------------------ -->
+    <script>
+      function formvalidate() {
+        var ptrn = /^([^0-9\W]*)$/;
+        if (ptrn.test(document.myform.uname.value)) {
+          document.getElementById("uname").textContent = "User Name is Valid";
+          document.getElementById("uname").style.background = "#72EF57";
+          document.getElementById("uname").style.fontSize = "12px";
+        } else {
+          document.getElementById("uname").textContent = "User Name is Invalid";
+          document.getElementById("uname").style.background = "#EF6257";
+          document.getElementById("uname").style.fontSize = "12px";
+        }
+      }
+
+      document.myform.uname.addEventListener("blur", formvalidate);
+    </script>
+  </body>
+</html>
+
+<!-- ---------Javascript for toggle menu------------- -->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.maxHeight = "0px";
+      function menutoggle() {
+        if (MenuItems.style.maxHeight == "0px") {
+          MenuItems.style.maxHeight = "200px";
+        } else {
+          MenuItems.style.maxHeight = "0px";
+        }
+      }
+    </script>
+  </body>
+</html>
+
+<!-- ---------Javascript for toggle menu------------- -->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.maxHeight = "0px";
+      function menutoggle() {
+        if (MenuItems.style.maxHeight == "0px") {
+          MenuItems.style.maxHeight = "200px";
+        } else {
+          MenuItems.style.maxHeight = "0px";
+        }
+      }
+    </script>
+  </body>
+</html>
+
+<!-- ---------Javascript for toggle menu------------- -->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.maxHeight = "0px";
+      function menutoggle() {
+        if (MenuItems.style.maxHeight == "0px") {
+          MenuItems.style.maxHeight = "200px";
+        } else {
+          MenuItems.style.maxHeight = "0px";
+        }
+      }
+    </script>
+  </body>
+</html>
+
+   <!-- ---------Javascript for toggle menu------------- -->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.maxHeight = "0px";
+      function menutoggle() {
+        if (MenuItems.style.maxHeight == "0px") {
+          MenuItems.style.maxHeight = "200px";
+        } else {
+          MenuItems.style.maxHeight = "0px";
+        }
+      }
+    </script>
+  </body>
+</html>
+
+
